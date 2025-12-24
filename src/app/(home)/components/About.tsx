060572722724b69b4c8314425bd558ad02d80f72
Komang Surya <komangsuryasedana@gmail.com>
2025-12-24 23:14:18 +0800

    feat: remove another code in about.tsx

--- src/app/(home)/components/About.tsx
+++ src/app/(home)/components/About.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { ScrollWrite } from "./ScrollWrite";
 
 export function About() {
@@ -35,102 +34,6 @@ export function About() {
           </h1>
         ))}
       </section>
-      <div className="px-4 md:px-8 py-10 bg-white">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          <div className="flex flex-col gap-6 md:mt-16">
-            <div className="flex gap-3">
-              <div className="relative w-1/4 aspect-2/3">
-                <Image
-                  src="/images/landing/hero5.webp"
-                  alt="Hero"
-                  fill
-                  className="object-cover"
-                />
-              </div>
-              <div className="relative w-2/5 aspect-3/2">
-                <Image
-                  src="/images/landing/hero5.webp"
-                  alt="Hero"
-                  fill
-                  className="object-cover"
-                />
-              </div>
-            </div>
-
-            <div className="flex justify-end gap-3 lg:mt-40">
-              <div className="relative w-2/3 aspect-square">
-                <Image
-                  src="/images/landing/hero5.webp"
-                  alt="Hero"
-                  fill
-                  className="object-cover"
-                />
-              </div>
-              <div className="relative w-1/4 h-1/2 aspect-2/3">
-                <Image
-                  src="/images/landing/hero5.webp"
-                  alt="Hero"
-                  fill
-                  className="object-cover"
-                />
-              </div>
-            </div>
-          </div>
-
-          <div className="h-[400px] md:h-[500px] lg:h-[800px] relative">
-            <div className="absolute inset-0 flex justify-center pointer-events-none">
-              <div className="sticky h-full w-px bg-neutral-500 z-0" />
-            </div>
-            <div className="flex flex-col gap-4 sticky top-18 z-10 bg-white py-4">
-              <h1 className="text-5xl lg:text-7xl font-marcellus text-center">
-                COBA <span className="text-sm -ml-2">TEMPLATE</span> <br />
-                TERBAIK<span className="ml-4">KAMI</span>
-              </h1>
-              <p className="text-center text-sm lg:mt-4 max-w-md mx-auto leading-relaxed">
-                Koleksi template undangan terbaik — elegan, berkelas, dan
-                dirancang dengan penuh makna. Tema-tema yang paling diminati,
-                dipercaya untuk menyempurnakan momen istimewa agar dapat
-                dibagikan dengan indah dan berkesan.
-              </p>
-            </div>
-          </div>
-
-          <div className="hidden md:block flex-col gap-6 lg:mt-16">
-            <div className="flex justify-end">
-              <div className="relative w-1/2 aspect-3/2">
-                <Image
-                  src="/images/landing/hero5.webp"
-                  alt="Hero"
-                  fill
-                  className="object-cover"
-                />
-              </div>
-            </div>
-
-            <div className="flex justify-center md:mt-24">
-              <div className="relative w-3/5 aspect-4/3 z-20">
-                <Image
-                  src="/images/landing/hero5.webp"
-                  alt="Hero"
-                  fill
-                  className="object-cover"
-                />
-              </div>
-            </div>
-
-            <div className="flex justify-end md:mt-4">
-              <div className="relative w-1/3 aspect-2/3">
-                <Image
-                  src="/images/landing/hero5.webp"
-                  alt="Hero"
-                  fill
-                  className="object-cover"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </>
   );
 }
